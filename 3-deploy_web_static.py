b87400646701cb0a531e0e07af4368a2686282e9
aabine <kendoaustine@yahoo.com>
2023-10-21 23:43:41 +0100

    ALX

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,40 +1,131 @@
+# #!/server/bin/python3
+# # Fabfile to create and distribute an archive to a web server.
+# import os.path
+# from datetime import datetime
+# from fabric.api import *
+# from fabric.contrib.files import exists
+
+# env.hosts = ['54.237.49.174', '54.146.60.252']
+
+# def do_pack():
+#     """Create a tar gzipped archive of the directory web_static."""
+#     dt = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+#     file = f"versions/web_static_{dt}.tgz"
+    
+#     os.makedirs("versions", exist_ok=True)
+    
+#     if local(f"tar -cvzf {file} web_static").failed is True:
+#         return None
+    
+#     return file
+
+
+# def do_deploy(archive_path):
+#     """Distributes an archive to a web server.
+
+#     Args:
+#         archive_path (str): The path of the archive to distribute.
+#     Returns:
+#         If the file doesn't exist at archive_path or an error occurs - False.
+#         Otherwise - True.
+#     """
+#     if not os.path.isfile(archive_path):
+#         return False
+
+#     file = os.path.basename(archive_path)
+#     name = os.path.splitext(file)[0]
+
+#     put_result = put(archive_path, f"/tmp/{file}")
+#     if put_result.failed:
+#         return False
+
+#     if exists(f"/data/web_static/releases/{name}"):
+#         run(f"sudo rm -rf /data/web_static/releases/{name}")
+#     if exists("/data/web_static/current"):
+#         run("sudo rm -rf /data/web_static/current")
+
+#     run(f"sudo mkdir -p /data/web_static/releases/{name}")
+#     run(f"sudo tar -xzf /tmp/{file} -C /data/web_static/releases/{name}")
+#     run(f"sudo rm /tmp/{file}")
+#     run(f"sudo mv /data/web_static/releases/{name}/web_static/* "
+#         f"/data/web_static/releases/{name}/")
+#     run(f"sudo rm -rf /data/web_static/releases/{name}/web_static")
+#     run(f"sudo ln -s /data/web_static/releases/{name} /data/web_static/current")
+#     return True
+
+
+# def deploy():
+#     """Create and distribute an archive to a web server."""
+#     file = do_pack()
+#     if file is None:
+#         return False
+#     return do_deploy(file)
+
 #!/server/bin/python3
-from fabric.api import run, put, local, env, task
+# Fabfile to create and distribute an archive to a web server.
+import os
 from datetime import datetime
+from fabric.api import *
+from fabric.contrib.files import exists
+import contextlib
 
-@task
+env.hosts = ['54.237.49.174', '54.146.60.252']
+
+@contextlib.contextmanager
+def temp_directory():
+    with hide('running', 'output'):
+        with settings(warn_only=True):
+            with cd("/tmp"):
+                dt = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+                temp_dir = f"temp_deploy_{dt}"
+                run(f"mkdir {temp_dir}")
+                try:
+                    yield temp_dir
+                finally:
+                    run(f"rm -rf {temp_dir}")
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    file = f"versions/web_static_{dt}.tgz"
+    
+    os.makedirs("versions", exist_ok=True)
+    
+    with settings(warn_only=True):
+        local(f"tar -cvzf {file} web_static")
+
+    return file if exists(file) else None
+
+def do_deploy(archive_path):
+    """Distribute an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if not os.path.isfile(archive_path):
+        return False
+
+    file = os.path.basename(archive_path)
+    name = os.path.splitext(file)[0]
+
+    with temp_directory() as temp_dir:
+        put(archive_path, f"{temp_dir}/{file}")
+        run(f"sudo rm -rf /data/web_static/releases/{name}")
+        run(f"sudo mkdir -p /data/web_static/releases/{name}")
+        run(f"sudo tar -xzf {temp_dir}/{file} -C /data/web_static/releases/{name}")
+        run(f"sudo rm {temp_dir}/{file}")
+        run(f"sudo mv /data/web_static/releases/{name}/web_static/* /data/web_static/releases/{name}/")
+        run(f"sudo rm -rf /data/web_static/releases/{name}/web_static")
+        run(f"sudo ln -s /data/web_static/releases/{name} /data/web_static/current")
+
+    return True
+
 def deploy():
-    """
-    Deploy your code to the remote servers
-    """
-    env.hosts = ['54.237.49.174', '54.146.60.252']
-    env.user = 'ubuntu'
-    env.key_filename = '~/.ssh/server-one'
-
-    remote_directory = '/opt/mydata'
-    remote_file = f'{remote_directory}/myfiles.tgz'
-
-    if run(f'test -f {remote_file}', warn=True).failed:
-        try:
-            now = datetime.now()
-            archive_name = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
-            local("mkdir -p versions")
-            local(f"tar -czvf versions/{archive_name} web_static")
-            archive_path = f"versions/{archive_name}"
-
-            if not exists(archive_path):
-                return False
-
-            put(archive_path, '/tmp/')
-            archive_name = archive_path.split('/')[-1]
-            archive_filename = archive_name[:-4]
-            release_path = f"/data/web_static/releases/{archive_filename}"
-            run(f"mkdir -p {release_path}")
-            run(f"tar -C {release_path} -xzvf /tmp/{archive_name}")
-            run(f"rm /tmp/{archive_name}")
-            run("rm -f /data/web_static/current")
-            run(f"ln -s {release_path} /data/web_static/current")
-            return True
-        except Exception as e:
-            print(f"Failed to deploy: {str(e)}")
-            return False
+    """Create and distribute an archive to a web server."""
+    file = do_pack()
+    if file is None:
+        return False
+    return do_deploy(file)
--- myscript.sh
+++ myscript.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
 # Update the package manager and install necessary development tools
-sudo dnf update -y
-sudo dnf groupinstall "Development Tools" -y
-
-# Install Python 3 and pip
-sudo dnf install python3 python3-pip -y
+sudo apt-get update
+sudo apt-get install -y build-essential libffi-dev libssl-dev python3-dev python3-pip python3-venv
 
 # Create a Python 3 virtual environment
-python3 -m venv myenv
+python3 -m venv server
 
 # Activate the virtual environment
-source myenv/bin/activate
+source server/bin/activate
 
 # Upgrade pip
 pip install --upgrade pip
 
 # Install required packages and dependencies
-    pip install pyparsing
-    pip install appdirs
-    pip install setuptools==40.1.0
-    pip install cryptography==2.8
-    pip install bcrypt==3.1.7
-    pip install PyNaCl==1.3.0
-    pip install Fabric3==1.14.post1
+pip install pyparsing appdirs setuptools==40.1.0 cryptography==2.8 bcrypt==3.1.7 PyNaCl==1.3.0 Fabric3==1.14.post1
 
 # Deactivate the virtual environment when you're done
 deactivate
