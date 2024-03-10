9b795471ce8509004c34d2d0e2513f2d9490cee6
devachref <bennadjiachref5@gmail.com>
2024-03-10 23:54:21 +0100

    send

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,21 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers
+"""
+
 from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.55.249.213", "54.157.32.137"]
+env.hosts = ["100.26.225.94 ", "34.229.55.246"]
 env.user = "ubuntu"
-env.key_filename = "my_ssh_private_key"
 
 
 def do_pack():
     """
-    return the archive path if archive has generated correctly.
+        return the archive path if archive has generated correctly.
     """
+
     local("mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     archived_f_path = "versions/web_static_{}.tgz".format(date)
@@ -24,17 +29,19 @@ def do_pack():
 
 def do_deploy(archive_path):
     """
-    Distribute archive.
+        Distribute archive.
     """
     if os.path.exists(archive_path):
-        archived_file = archive_path.split("/")[-1]
+        archived_file = archive_path[9:]
         newest_version = "/data/web_static/releases/" + archived_file[:-4]
         archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file, newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
         run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}/".format(newest_version, newest_version))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
         run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(newest_version))
@@ -43,4 +50,3 @@ def do_deploy(archive_path):
         return True
 
     return False
-
